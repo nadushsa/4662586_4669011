f6876bd015faffec82c5d622bcf5aabf55381939
nadushsa <nadushsa@outlook.com>
2023-06-28 00:47:18 +0530

    Initial commit

---
+++ app/src/main/java/com/vu/a4662586_4669011/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vu.a4662586_4669011;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
