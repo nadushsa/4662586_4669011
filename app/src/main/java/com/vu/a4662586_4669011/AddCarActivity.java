a30608414557c8cbcb31743d790be4674e863181
nadushsa <nadushsa@outlook.com>
2023-06-28 12:17:03 +0530

    Designed activity_main.xml

---
+++ app/src/main/java/com/vu/a4662586_4669011/AddCarActivity.java
@@ -0,0 +1,14 @@
+package com.vu.a4662586_4669011;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AddCarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_car);
+    }
+}
